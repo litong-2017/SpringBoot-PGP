ed39415783fee0260642a7550a414e6895575dee
logavdm <logavdm@gmail.com>
2019-06-25 18:54:09 +0530

    pgp welcome controller added

--- src/main/java/com/kambaa/main/PgpApplication.java
+++ src/main/java/com/kambaa/main/PgpApplication.java
@@ -2,10 +2,17 @@ package com.kambaa.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PgpApplication {
+public class PgpApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PgpApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PgpApplication.class, args);
 	}
--- src/main/java/com/kambaa/main/WelcomeController.java
+++ src/main/java/com/kambaa/main/WelcomeController.java
@@ -1,5 +1,8 @@
 package com.kambaa.main;
 
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +19,11 @@ public class WelcomeController {
 	@RequestMapping(value="/",method=RequestMethod.POST)
 	public String WelcomeControllerPOSTMethod() {	
 		return "Welcome to PGP encript service";
+	}
+	
+	@RequestMapping(value="/demo",method=RequestMethod.POST)
+	public Object DemoPostMethod(@RequestBody Map<String, Object> payload) {	
+		return payload;
 	}
 	
 }
